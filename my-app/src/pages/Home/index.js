2f6fa55435d4fec50c4b4a8b37709981a311cb13
Prasanna Gadal <gadalprasanna68@gmail.com>
2023-12-05 22:25:17 +0545

    landing page frontend completed

---
+++ my-app/src/pages/Home/index.js
@@ -0,0 +1,105 @@
+import NavBar from "@/Component/NavBar";
+import React, { useEffect } from "react";
+import Image from "next/image";
+import { Play } from "lucide-react";
+import Link from "next/link";
+import config from "../../config/index";
+import { useState } from "react";
+
+function Home() {
+  const [whatToDo, setWhatToDo] = useState({});
+  const [award, setAward] = useState({})
+  const fetchWhatToDo = async () => {
+    try {
+      const response = await fetch("/what-we-do.JSON");
+
+      if (!response.ok) {
+        throw new Error("Faild to fetch");
+      }
+
+      const data = await response.json();
+
+      setWhatToDo(data);
+    } catch (e) {
+      alert(e);
+    }
+  };
+
+  const designAwards = async() => {
+    try {
+      const response = await fetch("/award.json");
+
+      if (!response.ok) {
+        throw new Error("Faild to fetch");
+      }
+
+      const data = await response.json();
+
+      setAward(data);
+    } catch (e) {
+      alert(e);
+    }
+  }
+  useEffect(() => {
+    fetchWhatToDo();
+    designAwards();
+  }, []);
+
+  return (
+    <div>
+      <div className=" bg-green-700 " style={{ height: "75vh" }}>
+        <NavBar />
+        <h1 className=" mt-28 ml-36 text-white text-8xl">
+          Discover the <span className=" block">Best Furnitures</span>
+        </h1>
+        <p className=" mt-5 ml-36 text-white text-lg">
+          Take what you want for you dreamhouse to get complete. Not only your
+          will <span className=" block"></span> but too maximize your comfort
+          for relaxing homestay.
+        </p>
+        <div className=" mt-5 ml-36 text-white text-lg flex">
+          <button className="  bg-white text-green-700 font-bold w-40 p-2 text-lg rounded-xl">
+            Shop Now &#129170;
+          </button>
+          <Link
+            href={""}
+            className="  ml-5 bg-white text-green-700 font-bold  p-2 text-lg rounded-full "
+          >
+            <Play />
+          </Link>
+          <p className=" ml-2 mt-2">Play Video</p>
+        </div>
+      </div>
+      {/*What we Do Section*/}
+      <div className=" flex justify-center">
+        <div className=" flex  justify-between mt-16 w-3/4 p-20">
+          <h2 className=" text-7xl text-green-700">What We Do</h2>
+
+          <div className=" justify-items-end w-1/2">
+            <p className="  text-xl text-green-700">{whatToDo.content}</p>
+
+            <button className=" mt-10 bg-green-700 p-5 text-white font-bold rounded-2xl">
+              Learn More
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/*Award Section */}
+      <div className=" flex justify-center">
+        <div className="  flex  justify-between mt-16 bg-green-100 h-96 w-3/4 ">
+          <Image src={'/images/award.jpg'} height={384} width={600} />
+            <div className=" p-10">
+              <h2 className=" text-5xl text-green-700 text-left p-10">{award.tittle}</h2>
+              <p className=" text-lg text-green-700 p-10 text-left">{award.content}</p>
+            </div>
+        </div>
+      </div>
+
+
+    <h1 className=" text-center text-6xl text-green-700 p-20">Browse By Categories</h1>
+    </div>
+  );
+}
+
+export default Home;
